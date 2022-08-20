22a3892cf7c7f628fb31f47dd2b2a2da0e4337fe
Yuu <anpanmamZ3624@gmail.com>
2022-08-20 20:10:33 +0900

    enum型

---
+++ src/enum.ts
@@ -0,0 +1,20 @@
+export {};
+
+enum Months {
+  January,
+  February,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+console.log(Months.January);
+console.log(Months.March);
+console.log(Months.December)
