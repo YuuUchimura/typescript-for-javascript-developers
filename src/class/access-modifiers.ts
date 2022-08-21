601132694b07961beb3faa43ddc9872e1d3aef2f
Yuu <anpanmamZ3624@gmail.com>
2022-08-21 17:25:03 +0900

    access-modifiers

--- src/class/access-modifiers.ts
+++ src/class/access-modifiers.ts
@@ -1,12 +1,15 @@
 export {};
 
 class Person {
-  private name: string;
-  private age: number;
+  public name: string;
+  // private age: number;
+  protected age: number;
+  protected nationality: string;
 
-  constructor(name: string, age: number) {
+  constructor(name: string, age: number, nationality: string) {
     this.name = name;
     this.age = age;
+    this.nationality = nationality;
   }
 
   /**
@@ -17,6 +20,17 @@ class Person {
   }
 }
 
-let taro = new Person("taro", 24);
+class Android extends Person {
+  constructor(name: string, age: number, nationality: string) {
+    super(name, age, nationality);
+  }
 
+  public profile(): string {
+    return `name: ${this.name}, age: ${this.age}, nationality: ${this.nationality}`;
+  }
+}
+
+let taro = new Person("taro", 24, "Japan");
 console.log(taro.profile());
+// console.log(taro.name);
+// console.log(taro.age);
